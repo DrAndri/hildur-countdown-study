c12c7ef60aa51606a7caf01449dae1997e5f405c
Andri Þórhallsson <andrithorhalls@gmail.com>
2024-02-15 21:42:10 +0000

    countdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Countdown from "react-countdown";
 
 function App() {
+  const date = new Date("2024-05-24T11:00:00");
+  const renderer = (props) => {
+    if (props.completed) {
+      // Render a completed state
+      return "ÞÚ ERT BÚIN AÐ SKILA!!!";
+    } else {
+      // Render a countdown
+      return (
+        <span>
+          {props.formatted.days} : {props.formatted.hours} :{" "}
+          {props.formatted.minutes} : {props.formatted.seconds}
+        </span>
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countdown date={date} renderer={renderer} />
     </div>
   );
 }
